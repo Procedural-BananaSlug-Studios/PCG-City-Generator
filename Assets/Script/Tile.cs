a71d649e9e60d175407ac020d6b748717efc14b3
Jentsen <jentsenmaniti@gmail.com>
2024-03-19 02:44:12 -0700

    added two other buildings

--- Assets/Script/Tile.cs
+++ Assets/Script/Tile.cs
@@ -10,36 +10,41 @@ public class Tile : MonoBehaviour
     public Tile[] downNeighbour;
     public Tile[] leftNeighbour;
     public Tile[] rightNeighbour;
-    public void PrintNeighbors()
-    {
-        Debug.Log("Up Neighbors:");
-        foreach (Tile tile in upNeighbour)
-        {
-            Debug.Log(tile.name);
-        }
 
-        Debug.Log("Down Neighbors:");
-        foreach (Tile tile in downNeighbour)
-        {
-            Debug.Log(tile.name);
-        }
+    public float weight = 1.0f;
 
-        Debug.Log("Left Neighbors:");
-        foreach (Tile tile in leftNeighbour)
-        {
-            Debug.Log(tile.name);
-        }
+    // public void PrintNeighbors()
+    // {
+    //     Debug.Log("Up Neighbors:");
+    //     foreach (Tile tile in upNeighbour)
+    //     {
+    //         Debug.Log(tile.name);
+    //     }
 
-        Debug.Log("Right Neighbors:");
-        foreach (Tile tile in rightNeighbour)
-        {
-            Debug.Log(tile.name);
-        }
-    }
+    //     Debug.Log("Down Neighbors:");
+    //     foreach (Tile tile in downNeighbour)
+    //     {
+    //         Debug.Log(tile.name);
+    //     }
+
+    //     Debug.Log("Left Neighbors:");
+    //     foreach (Tile tile in leftNeighbour)
+    //     {
+    //         Debug.Log(tile.name);
+    //     }
+
+    //     Debug.Log("Right Neighbors:");
+    //     foreach (Tile tile in rightNeighbour)
+    //     {
+    //         Debug.Log(tile.name);
+    //     }
+    // }
 
     private void Awake()
     {
         transform.localScale = Vector3.zero;
         transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack);
     }
+
+
 }
